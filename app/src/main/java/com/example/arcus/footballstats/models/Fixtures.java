7d44db1d861927fc033ed4eccad26683681b90bf
SOliver93 <sasa.oliver@vsmti.hr>
2019-01-10 16:59:34 +0100

    upload 3

--- app/src/main/java/com/example/arcus/footballstats/models/Fixtures.java
+++ app/src/main/java/com/example/arcus/footballstats/models/Fixtures.java
@@ -1,4 +1,41 @@
 package com.example.arcus.footballstats.models;
 
 public class Fixtures {
+    public int matchday;
+    public short homeTeamName;
+    public short awayTeamName;
+    public int goalsHomeTeam;
+    public int goalsAwayTeam;
+
+    public int getMatchday() {
+        return matchday;
+    }
+    public short getHomeTeamName() {
+        return homeTeamName;
+    }
+    public short getAwayTeamName() {
+        return awayTeamName;
+    }
+    public int getGoalsHomeTeam() {
+        return goalsHomeTeam;
+    }
+    public int getGoalsAwayTeam() {
+        return goalsAwayTeam;
+    }
+
+    public void setmatchday(int matchday) {
+        this.matchday = matchday;
+    }
+    public void sethomeTeamName(short homeTeamName) {
+        this.homeTeamName = homeTeamName;
+    }
+    public void setawayTeamName(short awayTeamName) {
+        this.awayTeamName = awayTeamName;
+    }
+    public void setgoalsHomeTeam(int goalsHomeTeam) {
+        this.goalsHomeTeam = goalsHomeTeam;
+    }
+    public void setgoalsAwayTeam(int goalsAwayTeam) {
+        this.goalsAwayTeam = goalsAwayTeam;
+    }
 }
